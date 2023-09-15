2e9f6c2eaf1630e24d3c883798e2b814187bac6f
Thawon <topkmc@gmail.com>
2023-09-15 14:04:42 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Navbar } from "./component/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
+
+
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-[#8278D9] text-3xl font-bold underline">Hello world!</h1>
       <p>test</p>
+      <Navbar/>
     </>
   );
 }
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>Navbar
+
+<div>Navbar</div>
+<div>Navbar</div>
+<div>Navbar</div>
+<div>Navbar</div>
+<div>Navbar</div>
+<div>Navbar</div>
+<div>Navbar</div>
+<div>Navbar</div>
+<div>Navbar</div>
+<div>Navbar</div>
+    </div>
+  )
+}
