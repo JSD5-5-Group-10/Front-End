7f59f53b32ec30e57f3dca0cb98d09d337351054
Thawon <topkmc@gmail.com>
2023-09-15 15:36:56 +0700

    del component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Navbar } from "./component/Navbar";
+import React from "react";
+import { Navbar } from "./component/Navber";
 
 function App() {
-
-
-
   return (
     <>
+      <Navbar/>
       <h1 className="text-[#8278D9] text-3xl font-bold underline">Hello world!</h1>
       <p>test</p>
-      <Navbar/>
+      
     </>
   );
 }
