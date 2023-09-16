bcb7970d06f24cc9cf219d1399be2bd26dbc1ead
Thawon <topkmc@gmail.com>
2023-09-16 18:54:59 +0700

    update login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import ActivityForm from "./component/Activity";
 import { Dashboard } from "./component/Dashboard";
 import { Navbar } from "./component/Navber";
+import { Login } from "./component/login-registration/Login";
+import { Registration } from "./component/login-registration/Registration";
 
 
 function App() {
   return (
     <>
+      <Login/>
+      <Registration/>
       {/* <Navbar /> */}
       <Dashboard />
       {/* <ActivityForm /> */}
