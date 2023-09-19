4a1072fc353e01dd0a61f2a37c78e5e826b2eba1
AliceLs025 <kokonut280741@gmail.com>
2023-09-19 15:03:33 +0700

    add activity

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 
 import ActivityForm from '../src/component/Activity/ActivityForm'
-import IndexActivity from './component/Activity';
+import IndexActivity from './component/Activity/IndexActivity';
 import Navbar from "./component/Navbar";
 import { Index } from './pages/Index/Index';
 
@@ -16,6 +16,7 @@ function App() {
       <div className="min-h-min z-0 relative">
         <Index />
       </div>
+      <IndexActivity />
     </div>
   )
 
