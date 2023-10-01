141f757874673ef24641bc9003b61b2bbb8f626a
auyatip <atipthummakul@gmail.com>
2023-10-01 16:02:41 +0700

    Change Col-2

--- src/component/Activity/IndexActivity.jsx
+++ src/component/Activity/IndexActivity.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import { BiRun } from "react-icons/bi";
 import { GrYoga } from "react-icons/gr";
-import { GiJumpingRope } from 'react-icons/gi'
+import { GiJumpingRope } from "react-icons/gi";
 import Imgyoga from "./assets/yoga.jpg";
 import Running from "./assets/running.jpg";
 import Arabic from "./assets/Arabic.jpg";
-
 
 const test = [
   {
@@ -29,8 +28,8 @@ const test = [
     enddate: "2023-09-03",
     time: 60,
     weight: 60,
-    icon: <GiJumpingRope size={100}/>,
-    img: Arabic
+    icon: <GiJumpingRope size={100} />,
+    img: Arabic,
   },
   {
     id: 3,
@@ -57,10 +56,10 @@ const test = [
     img: Imgyoga,
   },
 ];
-  
-const IndexActivity = ({value}) => {
+
+const IndexActivity = ({ value }) => {
   const [data, setData] = useState(test);
-console.log(value);
+  console.log(value);
   return (
     <div className="flex flex-col items-center h-full">
       <div className="lg:grid lg:grid-cols-2 xl:grid-cols-2">
@@ -68,20 +67,23 @@ console.log(value);
           <div
             key={index}
             className="w-[350px] h-[200px] m-4 shadow-xl border border-[#827BD9] relative rounded-2xl flex flex-row  text-ellipsisp overflow-hidden hover:bg-[#827BD9] hover:h-[400px] hover:shadow-[#827bd9] hover:drop-shadow-2xl duration-700
-            lg:h-[400px] lg:hover:scale-105"
+            lg:h-[400px] lg:hover:scale-105 "
           >
             <div className="p-2 mr-3 flex items-center absolute ">
               {item.icon}
             </div>
             {/* Overlay */}
             <div className="absolute w-full h-full bg-black/60 text-white rounded-xl flex flex-col items-center justify-center ">
-              
               <div className=" mb-1">Activity Type : {item.type}</div>
               <div className="mb-1">Activity Name : {item.name}</div>
               <div className=" mb-1"> Date : {item.startdate} mins.</div>
-              <div className="hidden lg:inline">Description : {item.descrition}</div>
+              <div className="hidden hover:inline-block lg:inline">
+                Description : {item.descrition}
+              </div>
               <div className="w-full">
-                <h2 className="uppercase absolute bottom-1 right-3 underline">Total Cal: XXX</h2>
+                <h2 className="uppercase absolute bottom-1 right-3 underline">
+                  Total Cal: XXX
+                </h2>
               </div>
             </div>
 
