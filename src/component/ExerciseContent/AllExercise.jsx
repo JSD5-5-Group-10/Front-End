bc2fc7c3545874e81f01234a058be7cc8b4457d4
Thawon <topkmc@gmail.com>
2023-10-01 16:15:12 +0700

    test

--- src/component/ExerciseContent/AllExercise.jsx
+++ src/component/ExerciseContent/AllExercise.jsx
@@ -1,66 +1,45 @@
 import React from "react";
 
-class ExerciseCard {
-  constructor(title, imgSrc, altText, description) {
-    this.title = title;
-    this.imgSrc = imgSrc;
-    this.altText = altText;
-    this.description = description;
-  }
-
-  render() {
-    return (
-      <div className="relative w-[300px] md:w-auto md:h-auto">
-        <img
-          src={this.imgSrc}
-          alt={this.altText}
-          className="rounded-2xl opacity-70 hover:shadow-md hover:shadow-[#8278D9] hover:opacity-100"
-        />
-        <div className="textContainer">
-          <h3 className="absolute text-3xl md:text-5xl text-white font-medium top-1/3 left-1/3 -translate-x-1/2 -translate-y-1/2">
-            {this.title}
-          </h3>
-          <p className="absolute hidden md:inline text-base text-center italic max-w-[100%] m-1">
-            {this.description}
-          </p>
-        </div>
-      </div>
-    );
-  }
-}
-
 function AllExercise() {
-  const exercises = [
-    new ExerciseCard(
-      "Yoga",
-      "https://drive.google.com/uc?id=1ac0TttcdlGiTQJqTa9aSnepGii6xeJXy",
-      "yoga img",
-      "Spiritual discipline based on a science, which focuses on mind and body"
-    ),
-    new ExerciseCard(
-      "Running",
-      "https://drive.google.com/uc?id=1fyjE-yDQwA0QOVK6lukz8WDZMN1vwpXx",
-      "running img",
-      "Foot racing over a variety of distances and courses, popular sports in nearly all times and places"
-    ),
-    new ExerciseCard (
-      "Thai Boxing",
-      "https://drive.google.com/uc?id=1sUyURiEbC6jvFeVkfiCey8fJ5xhMvLH7",
-      "boxing img",
-      "Combat Sport (martial art) has been developed in Thailand"
-    ), 
-    new ExerciseCard (
-      "Weight Training",
-      "https://drive.google.com/uc?id=1r8mkjX6rfXWrvAYO5jBgBw1N1c8XYnnv",
-      "weight img",
-      "System of physical conditioning using free weights"
-    ), 
-    new ExerciseCard (
-      "Aerobics",
-      "https://drive.google.com/uc?id=1mAgZEdxYEBU4EOhWFPWG_72zVpRYfO4u",
-      "aerobics img",
-      "Physical exercise of low to high intensity that depends on the aerobic energy-generating process"
-    ), 
+  const data = [
+    {
+      title: "Yoga",
+      imgSrc:
+        "https://drive.google.com/uc?id=1ac0TttcdlGiTQJqTa9aSnepGii6xeJXy",
+      altText: "yoga img",
+      description:
+        "Spiritual discipline based on a science, which focuses on mind and body",
+    },
+    {
+      title: "Running",
+      imgSrc:
+        "https://drive.google.com/uc?id=1fyjE-yDQwA0QOVK6lukz8WDZMN1vwpXx",
+      altText: "running img",
+      description:
+        "Foot racing over a variety of distances and courses, popular sports in nearly all times and places",
+    },
+    {
+      title: "Thai Boxing",
+      imgSrc:
+        "https://drive.google.com/uc?id=1sUyURiEbC6jvFeVkfiCey8fJ5xhMvLH7",
+      altText: "boxing img",
+      description: "Combat Sport (martial art) has been developed in Thailand",
+    },
+    {
+      title: "Weight Training",
+      imgSrc:
+        "https://drive.google.com/uc?id=1r8mkjX6rfXWrvAYO5jBgBw1N1c8XYnnv",
+      altText: "weight img",
+      description: "System of physical conditioning using free weights",
+    },
+    {
+      title: "Aerobics",
+      imgSrc:
+        "https://drive.google.com/uc?id=1mAgZEdxYEBU4EOhWFPWG_72zVpRYfO4u",
+      altText: "aerobics img",
+      description:
+        "Physical exercise of low to high intensity that depends on the aerobic energy-generating process",
+    },
   ];
 
   return (
@@ -69,13 +48,26 @@ function AllExercise() {
         Exercise Content
       </h1>
       <div className="place-items-center grid grid-cols-1 gap-5 md:grid-cols-3 md:mx-[10px] md:gap-y-[110px] lg:gap-y-[80px]">
-        {exercises.map((exercise, index) => (
-          <div key={index}>{exercise.render()}</div>
+        {data.map((item, index) => (
+          <div className="relative w-[300px] md:w-auto md:h-auto">
+            <img
+              src={item.imgSrc}
+              alt={item.altText}
+              className="rounded-2xl opacity-70 hover:shadow-md hover:shadow-[#8278D9] hover:opacity-100"
+            />
+            <div className="textContainer">
+              <h3 className="absolute text-3xl md:text-5xl text-white font-medium top-1/3 left-1/3 -translate-x-1/2 -translate-y-1/2">
+                {item.item}
+              </h3>
+              <p className="absolute hidden md:inline text-base text-center italic max-w-[100%] m-1">
+                {item.description}
+              </p>
+            </div>
+          </div>
         ))}
       </div>
     </div>
   );
 }
 
-export default AllExercise
-
+export default AllExercise;
