41f76f7b7e70329db74d4f90d4c387063f1db3d7
siwatoa <siwatoa@gmail.com>
2023-10-22 18:20:00 +0700

    create folder AboutUs in component folder

---
+++ src/component/AboutUs/AboutUs.js
@@ -0,0 +1,57 @@
+import yogalogo from './assets/yogalogo.png'
+import runlogo from './assets/runlogo.png';
+import boxinglogo from './assets/boxinglogo.png';
+import weightlogo from './assets/weightlogo.png';
+import aerobiclogo from './assets/aerobiclogo.png';
+
+export const AboutA = {
+    head: "WELCOME TO FITBOD",
+    slogan1: "FIT YOUR GOAL",
+    slogan2: "YOUR MIND AND BODY",
+    des1: "Pick from the 5 workouts",
+    des2: "Yoga, Running, Thai-Boxing, Weight Training, and Aerobics",
+    des3: "For a complete mind and body boost towards feeling fantastic",
+};
+
+export const AboutImgSet =[
+    {img: yogalogo},
+    {img: runlogo},
+    {img: boxinglogo},
+    {img: weightlogo},
+    {img: aerobiclogo},
+];
+
+export const Prof1 = {
+    head: "Thawon Jatuwat (Top)",
+    link1: "https://www.linkedin.com/in/thawon/",
+    link2: "https://tp88x.eth.limo/" ,
+    link3: "https://github.com/TP88x",
+};
+
+export const Prof2 = {
+    head: "Chalida Thewarothon (Ploy)",
+    link1:"https://www.linkedin.com/in/chalida-thewarothon/",
+    link2:"/",
+    link3:"/",
+};
+
+export const Prof3 = {
+    head: "Athip Thumakul (Auy)",
+    link1:"https://www.linkedin.com/in/athip-thumakul/",
+    link2:"https://portfolio-auyatip.vercel.app/",
+    link3:"/",
+};
+
+export const Prof4 = {
+    head: "Siwat Photinam (Tack)",
+    link1:"https://www.linkedin.com/in/siwat-photinam/",
+    link2:"/",
+    link3:"https://github.com/siwatoa",
+};
+
+export const Prof5 = {
+    head: "Patit Sudsat (Gear)",
+    link1:"/",
+    link2:"https://script.google.com/macros/s/AKfycbxACdhEIXRNQh0rwG0b7gosklyKy9llcH1GwXc5jdvJasiVAuXustxRnpohYiZ2CWUE/exec?fbclid=IwAR0wAUaxGxGRJZ67RQe6Myt1pyQXF7KKm8iHWs1-qLGgu1sgUaKlchq2uYI",
+    link3:"/",
+};
