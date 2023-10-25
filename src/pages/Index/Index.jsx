93aea2bd4df94c1230541ce0d3ea7ab020eac4e8
Thawon <topkmc@gmail.com>
2023-10-25 20:23:07 +0700

    update css

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -8,13 +8,12 @@ export const Index = () => {
   return (
     <div className="flex justify-center min-h-[1000px] h-[1200px] md:h-[1300px] lg:h-[1200px] xl:h-screen dark:text-cyan-50 text-black dark:bg-gray-800 bg-white">
       <div className="flex w-full max-w-[1380px] md:flex md:h-screen">
-        
         <div className="z-10 h-[1200px] max-h-screen md:static">
           <Navbar />
         </div>
 
         <div className="w-full h-full z-0 mt-10 mb-10 ">
-          <div className="flex justify-center items-center  dark:text-cyan-50 text-black dark:bg-gray-800 bg-white ">
+          <div className="flex justify-center items-center dark:text-cyan-50 text-black dark:bg-gray-800 bg-white ">
             <Link
               to="/ActivityForm"
               className=" flex items-center rounded-full hover:scale-105 hover:drop-shadow-md hover:text-black rounded-bl-lg hover:animate-bounce duration-200 dark:hover:text-[#5446C9] border-solid border-2 border-[#4351CC] z-20 "
